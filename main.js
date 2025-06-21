15c2c03a0c9ab1d5a68a4a48e217b54c36f83e0d
bazirake <bazirakeric1@gmail.com>
2025-06-21 20:32:10 +0100

    changes api messages

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const express=require("express");
 const getConnection = require("./db_config");
 const app=express();
+//JSON parser middleware
 app.use(express.json());
 const runningp=3000;
 
@@ -72,74 +73,64 @@ app.get("/content/:id",(req,res)=>{
     if (err) {
         res.send("err"+err);
     } else{
-         res.send(result.rows)
+       res.send(result.rows)
     }
 })
 });
+ app.post("/student",(req,res)=>{
+  const{fname,email,tel,country,terms,cid,conid}=req.body;
+  const student='INSERT INTO public.student(fname,email,tel,country,terms,cid,conid) VALUES ($1,$2,$3,$4,$5,$6,$7)';
+  conn.query(student,[fname,email,tel,country,terms,cid,conid],(err,result)=>{
+   if (err) {
+    res.send(err);
+    
+   } else {
+    res.send({message:"Course applied successfully"});
+   }
+  });
+});
 
+app.get("/student-course",(req,res)=>{
+    const stcourse='SELECT id, fname, email, tel, country, terms, cid, conid FROM public.student';
+    conn.query(stcourse,(err,result)=>{
+      if (err) {
+        res.send({"errorm":err});
+        
+      } else {
+        res.send(result.rows);
+      }
+    });
+});
+
+app.post("/contact",(req,res)=>{
+ const{name,email,subjects,message}=req.body;
+ const contact='INSERT INTO public.contacts(name,email,subjects,message) VALUES($1,$2,$3,$4)';
+ conn.query(contact,[name,email,subjects,message],(err,result)=>{
+  if (err){
+    res.send({"errormessage":err})
+  } else {
+    res.send({"message":"message has been sent"})
+  }
+ })
+});
+
+app.get("/get-contact",(req,res)=>{
+    const contact='SELECT id,name,email,subjects,message FROM public.contacts';
+    conn.query(contact,(err,result)=>{
+       if (err) {
+        res.send({"errorMessage":err});
+       } else {
+        res.send(result.rows);
+       }
+
+    });
+});
 const PORT=runningp;
  app.listen(PORT,()=>{
     console.log(`server is running http://localhost:${PORT}`);
 });
 
 
-
-//    app.post("/create-course",(req,res)=>{
-//     const {title,subtitle,content,subcontent,cid}=req.body;
-   
-//     const courses='INSERT INTO public.course(title, subtitle, content, subcontent,cid) VALUES ($1,$2,$3,$4,$5)';
-//     conn.query(courses,[title,subtitle,content,subcontent,cid],(err,resultX)=>{
-//     if(err) {
-//     res.status(500).send(err);
-// } else {
-//     res.send("Now course has been created");
-// }
-//   });
-// });
-
-
-// app.get("/category",(req,res)=>{
-//  const categorys='SELECT name,id from public.category';
-//  conn.query(categorys,(err,result)=>{
-//      if(err) {
-//       res.send(err);
-//      } else {
-//      res.send(result.rows);
-//      }
-//  })
-// })
-
-// app.get("/getda",(req,res)=>{
-//  res.send("hello data APIxxx");
-// })
-
-// app.get("/course-content",(req,res)=>{
-//    const coursecontent= 'SELECT title, subtitle, content, subcontent, id, cid FROM public.course';
-//    conn.query(coursecontent,(err,result)=>{
-//      if(err){
-//          res.send('Dabase error:'+err);
-//      }else{
-//          res.status(200).json(result.rows);
-//      }
-//    });
-// });
-
-// app.get("/course-content/:id",(req,res)=>{
-//     const id=req.params.id;
-//     const coursecontent= 'SELECT title, subtitle, content, subcontent, id, cid FROM public.course where cid=$1';
-//     conn.query(coursecontent,[id],(err,result)=>{
-//      if (err) {
-//          res.send('Dabase error:'+err)
-//      } else {
-//          res.status(200).send(result.rows);
-//      }
-//    });
-// });
-
-
-//  app.listen(process.env.PORT || 3000,()=>{
-//      console.log("Server is running");
-// });
 
 
 
