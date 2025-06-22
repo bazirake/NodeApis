2a858f469e1f0595b252e1e1c29207b5090072af
bazirake <bazirakeric1@gmail.com>
2025-06-22 17:12:34 +0100

    changes data

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ app.get("/student-course",(req,res)=>{
     const stcourse='SELECT id, fname, email, tel, country, terms, cid, conid FROM public.student';
     conn.query(stcourse,(err,result)=>{
       if (err) {
-        res.send({"errorm":err});
+        res.send({errorm:err});
       } else {
         res.send(result.rows);
       }
@@ -98,9 +98,9 @@ app.post("/contact",(req,res)=>{
  const{name,email,subjects,message}=req.body;
  const contact='INSERT INTO public.contacts(name,email,subjects,message) VALUES($1,$2,$3,$4)';
  conn.query(contact,[name,email,subjects,message],(err,result)=>{
-  if (err){
-    res.send({"errormessage":err})
-  } else{
+ if(err){
+    res.send({errormessage:err})
+  }else{
     res.send({message:"message has been sent"})
   }
  })
@@ -110,7 +110,7 @@ app.get("/get-contact",(req,res)=>{
     const contact='SELECT id,name,email,subjects,message FROM public.contacts';
     conn.query(contact,(err,result)=>{
        if (err) {
-        res.send({"errorMessage":err});
+        res.send({errorMessage:err});
        } else {
         res.send(result.rows);
        }
